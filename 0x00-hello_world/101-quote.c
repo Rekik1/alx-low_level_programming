775eb676e789cd2d8bc5708018805eabdf757ebf
Rekik1 <rik939@gimal.com>
2023-02-16 11:53:22 -0800

    Write a C program that prints exactly and that piece of art is useful - Dora Korpar 2015-10-19 followed by a new line to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
-*main - Write a C program that prints a line usung -wall gcc option
-*Return: 1 (Success)
+*main -Entry point
+*Return: 1
 */
+
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", Stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
+
