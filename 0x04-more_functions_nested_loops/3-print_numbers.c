a6ca39a05063f3e3e6de573dd090aa37a95994b8
Rekik1 <rik939@gimal.com>
2023-02-23 14:41:25 -0800

    The numbers speak for themselves  prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * print_numbers - checks for a digit.
 *
@@ -7,11 +6,10 @@
 */
 void print_numbers(void)
 {
-
 int a;
 for (a = 0; a <= 9; a++)
-
+{
 _putchar(a + ‘0’);
+}
 _putchar('\n');
 }
-
