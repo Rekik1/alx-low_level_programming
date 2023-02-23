b8fcdb5392c562d883dd8cc795c80dc6d89cf71e
Rekik1 <rik939@gimal.com>
2023-02-23 12:52:11 -0800

    The numbers speak for themselves.Write a function that prints the numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,14 @@
 #include "main.h"
-
 /**
 * print_numbers - checks for a digit.
 *
-* Return: Always 0.
+* Return: 0 to 9
 */
-
 void print_numbers(void)
-
 {
 int a;
-
 for (a = 0; a <= 9; a++)
-{
+
 _putchar(a + ‘0’);
-}
 _putchar('\n');
 }
